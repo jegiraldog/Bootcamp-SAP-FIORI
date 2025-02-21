c914d3972069cfb41d8210696fd0c1895a60ad34
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2025-02-21 21:07:56 +0000

    6_Binding_Detail_Page

--- freestyle/webapp/controller/Detail.controller.js
+++ freestyle/webapp/controller/Detail.controller.js
@@ -6,6 +6,21 @@ sap.ui.define([
 
     return Controller.extend("com.bootcamp.sapui5.freestyle.controller.Detail", {
         onInit() {
-        }
+            let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            // Obtener el ProductID de la URL y enlazar el contexto
+            let sProductID = oEvent.getParameter("arguments").ProductID;
+
+
+            this.getView().bindElement({
+                path: "/Products(" + sProductID + ")",
+                parameters: {
+                    expand: "Order_Details"
+                }
+            });
+        },
     });
 });
