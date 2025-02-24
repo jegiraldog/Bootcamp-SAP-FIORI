dc6498a34356eff3150c1bfbba91ac9f6afad6af
Jhony Esteban Giraldo Castaño <jhony.giraldo@globant.com>
2025-02-24 15:10:37 +0000

    Corrección Nombre función getProposalBystatus -> getDataProducts

--- freestyle/webapp/controller/Home.controller.js
+++ freestyle/webapp/controller/Home.controller.js
@@ -10,7 +10,7 @@ sap.ui.define([
         },
 
         onPress: async function(){
-            let oDatos = await HomeHelper.getProposalBystatus(); 
+            let oDatos = await HomeHelper.getDataProducts(); 
             await HomeHelper.setProductModel(this, oDatos[0].results);
         },
 
